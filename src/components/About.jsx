58708bc7f57e5ed2e504a910be693d8b8889d3dc
Dota43ver <facundoagueroc@gmail.com>
2023-05-22 14:59:09 -0300

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt tenetur alias ea voluptate quis quos libero. Facere veritatis eveniet iure hic. Aliquid perferendis quibusdam, et ab nostrum, eveniet quos cupiditate, dolore harum ipsum doloribus. Nihil laudantium adipisci unde, rem repudiandae quis, aliquam, officia ut totam vel id expedita? Nisi?
+            </p>
+            <br />
+            <p className='text-xl'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius voluptatem a voluptate dolorum mollitia! Nihil impedit odit libero iusto pariatur? Explicabo sint est possimus velit voluptas laudantium adipisci accusantium assumenda? Perspiciatis corrupti saepe, quos esse sequi neque doloribus minus fugit. Nulla iste excepturi facilis voluptatum omnis quo, quos incidunt vero.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Oasis from "../assets/portfolio/Oasis.JPG";
+import DogApp from "../assets/portfolio/DogApp.JPG"
+
+
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: DogApp,
+      demo: "https://pi-dogs-fuj6.vercel.app/",
+      code: "https://github.com/Dota43ver/PI-Dogs"
+    },
+    {
+      id: 2,
+      src: Oasis,
+      demo: "https://oasis-library.vercel.app/",
+      code: "https://github.com/Dota43ver/OasisLibrary"
+    },
+    
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src,demo,code }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105 w-[500px] h-[200px] "
+              />
+              <div className="flex items-center justify-center">
+                <a href={demo} target="_blank" rel="noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 flex items-center justify-center">
+                  Demo
+                </a>
+                <a href={code} target="_blank" rel="noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 flex items-center justify-center">
+                  Code
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
